177c7ce6ad85dc49d49689ddaf333e4f4a65b3be
Michael Brooks and Senya Webster <senya@>
2017-11-30 09:42:02 -0800

    add working nav bar and wrap up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,34 @@
 Rails.application.routes.draw do
+  get 'test/new'
+
+  get 'test/create'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users do
+    resources :photos
+  end
+
+  # resources :users do
+  #   resources :comments
+  # end
+  #
+  # resources :users do
+  #   resources :tags
+  # end
+
+  resources :users do
+    resources :favorites
+  end
+
+  resources :photos do
+    resources :tags
+  end
+
+  resources :photos do
+    resources :comments
+  end
 
   root :to => 'photos#index'
 end
